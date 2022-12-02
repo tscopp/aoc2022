97ce8ecaefc2a1bb2bc569ce40d407161e323f10
tms <tscoppetta@zynga.com>
2022-12-01 23:20:57 -0800

    assign all the zeroes on one line

--- 1/top3.py
+++ 1/top3.py
@@ -1,7 +1,7 @@
 f = open("input.txt", "r")
-elf_counter = 0
-elf_calories = 0
+elf_counter, elf_calories, top3_carry = 0, 0, 0
 elf_carry = []
+
 for line in f:
     if len(line) == 1:
         elf_carry.append(int(elf_calories))
@@ -10,7 +10,6 @@ for line in f:
     else:
         elf_calories = elf_calories + int(line)
 
-top3_carry = 0
 for carry in sorted(elf_carry)[-3:]:
     top3_carry = top3_carry + carry
 
