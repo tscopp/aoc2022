4a40181a0a23150cb2be23a5e04196169ce5d0dd
tms <tscoppetta@zynga.com>
2022-12-05 21:37:27 -0800

    Thats a little bit better

--- 6/som.py
+++ 6/som.py
@@ -7,23 +7,11 @@ char_counter = 0
 for character in input_list:
     char_list = []
     try:
-        char_list.append(input_list[char_counter])
-        char_list.append(input_list[char_counter + 1])
-        char_list.append(input_list[char_counter + 2])
-        char_list.append(input_list[char_counter + 3])
-        char_list.append(input_list[char_counter + 4])
-        char_list.append(input_list[char_counter + 5])
-        char_list.append(input_list[char_counter + 6])
-        char_list.append(input_list[char_counter + 7])
-        char_list.append(input_list[char_counter + 8])
-        char_list.append(input_list[char_counter + 9])
-        char_list.append(input_list[char_counter + 10])
-        char_list.append(input_list[char_counter + 11])
-        char_list.append(input_list[char_counter + 12])
-        char_list.append(input_list[char_counter + 13])
+        for i in range(0, 14):
+            char_list.append(input_list[char_counter + i])
         if len(set(char_list)) == len(char_list):
             print(f"Unique: {char_list}")
-            char_counter += 14
+            char_counter += len(char_list) 
             break
         else:
             print(f"Not Unique: {char_list}")
@@ -34,5 +22,3 @@ for character in input_list:
     char_counter += 1
 
 print(char_counter)
-
-
